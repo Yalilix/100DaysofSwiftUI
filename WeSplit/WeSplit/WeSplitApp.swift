efa702c9e6cfb6a923b9cff1cc1dae3b06ab0fab
Yanlin Li <yanauslin8@gmail.com>
2025-09-06 15:05:07 +1000

    Stop tracking UserInterfaceState.xcuserstate

---
+++ WeSplit/WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Yanlin Li  on 1/9/2025.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
